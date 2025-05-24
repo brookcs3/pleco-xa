6d3cb926084f1a62ce96143556b6314519444ea4
brookcs3 <brooksc3@oregonstate.edu>
2025-05-24 01:15:41 -0700

    fa

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,10 @@
 module.exports = {
   testEnvironment: "node",
-
-  transform: {},
-
-
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+    "^.+\\.ts$": "ts-jest"
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!dependency1|dependency2|dependency3)/"
+  ]
 };
