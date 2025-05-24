c0bbc5898a9c67b5a542b62475fe458954799460
brookcs3 <brooksc3@oregonstate.edu>
2025-05-24 01:18:49 -0700

    as

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,6 @@
-module.exports = {
+import { describe, it, expect } from "@jest/globals";
+
+export default {
   testEnvironment: "node",
   transform: {
     "^.+\\.[jt]sx?$": "babel-jest",
@@ -8,3 +10,12 @@ module.exports = {
     "/node_modules/(?!dependency1|dependency2|dependency3)/"
   ]
 };
+
+{
+  "compilerOptions": {
+    "target": "ES6",
+    "module": "commonjs",
+    "strict": true,
+    "esModuleInterop": true
+  }
+}
