81d34f0692852643ad41caebdc73f94457c40e96
Cameron Brooks <brooksc3@oregonstate.edu>
2025-05-24 01:27:01 -0700

    Ignore bun tests in Jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   testEnvironment: "node",
 
+  // Ignore example backend tests that rely on Bun's test runner
+  testPathIgnorePatterns: ["<rootDir>/examples/backend/tests"],
+
   transform: {},
 
 
