1b1cdef5239cbe55dd965d74a8ddc1aeaaabf41b
Cameron Brooks <brooksc3@oregonstate.edu>
2025-05-24 23:07:03 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,46 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  output: 'static',
+  site: 'https://pleco-xa.com',
+
+  scopedStyleStrategy: 'class',
+
+  server: {
+    host: true,
+    port: 3000
+  },
+
   publicDir: 'public',
   outDir: 'dist',
-  server: {
-    port: 3000,
-    host: true
-  },
+
   build: {
     assets: 'assets'
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use 'sass:math'; @use 'sass:map'; @use "@/styles/import" as *;`
+        }
+      }
+    },
+    build: {
+      assetsInlineLimit: 0
+    }
+  },
+
+  devToolbar: {
+    enabled: false
   }
 });
